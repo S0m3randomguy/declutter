b06ed5f7ee270ac3f9e695b64750792db5575866
Sarge <63020240+S0m3randomguy@users.noreply.github.com>
2023-03-25 17:23:17 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,42 @@
 
+/**
+ * Declutter your blocks workspace with a sprite Stack
+ * that can be manipulated
+ */
+//% color=#4045db weight=0 icon="\uf056" block="Declutter"
+namespace declutter {
+    let stack = new Stack();
+
+    //% block="load sprite $sprite with name $name"
+    //% blockId=declutter_load_sprite
+    //% sprite.shadow=spritescreate
+    //% name.defl="name"
+    //% weight=100
+    export function load(name: string, sprite: Sprite): void {
+        stack.load(name, sprite);
+    }
+
+    //% block="get sprite with name $name"
+    //% blockId=declutter_get_sprite
+    //% name.defl="name"
+    //% weight=90
+    export function get(name: string): Sprite {
+        return stack.get(name);
+    }
+
+    //% block="offload sprite with name $name"
+    //% blockId=declutter_offload_sprite
+    //% name.defl="name"
+    //% weight=80
+    export function offload(name: string): void {
+        stack.offload(name);
+    }
+
+    //% block="stack contains sprite with name $name"
+    //% blockId=declutter_contains_sprite
+    //% name.defl="name"
+    //% weight=70
+    export function contains(name: string): boolean {
+        return stack.contains(name);
+    }
+}
